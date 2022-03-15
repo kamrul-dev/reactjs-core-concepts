286020f96fe19e1a67169d4965d735561f730986
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-15 21:31:28 +0600

    write a function and create my own tag

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 5555;
-const singer = {name: 'James', job: 'Singer'};
-const singer2 = {name: 'Runa Laila', job: 'Singer2'};
+const singer = { name: 'James', job: 'Singer' };
+const singer2 = { name: 'Runa Laila', job: 'Singer2' };
 
 const singerStyle = {
   color: 'purple',
@@ -13,31 +13,14 @@ const singerStyle = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello React . How are you?</h3>
-        </div>
-        <div className="music">
-          <p>Name: {number}</p>
-          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
-          <p style={{color: 'blue', backgroundColor: 'yellow'}}>Name: {singer2.name} {singer2.job}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
     </div>
   );
+}
+
+function Person() {
+  return <h1>Sakib Al Hasan</h1>
 }
 
 export default App;
